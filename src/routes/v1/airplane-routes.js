698f9b65e6a00aa32e8f1ff423193fcb2923126e
Sourabh Kumar Singh <sksas9831@gmail.com>
2026-04-17 11:21:30 +0530

    Add airplane retrieval functionality and CRUD repository improvements
    - Implemented getAirplanes function in AirplaneController to fetch all airplanes.
    - Added corresponding route for GET request to /api/v1/airplanes.
    - Enhanced CRUD operations in CrudRepository by removing redundant error handling.
    - Introduced seeder for adding initial airplane data to the database.

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -4,9 +4,16 @@ const { AirplaneController } = require('../../controllers');
 const { AirplaneMiddlewares } = require('../../middlewares')
 const router = express.Router();
 
+// /api/v1/airplanes POST 
 router
     .post('/', 
         AirplaneMiddlewares.validateCreateRequest,
-        AirplaneController.createAirplane);
+        AirplaneController.createAirplane
+    );
 
+// /api/v1/airplanes GET
+router
+    .get('/',
+        AirplaneController.getAirplanes
+    );
 module.exports = router;
